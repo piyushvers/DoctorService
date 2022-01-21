59ee44cb7cbadadfa03978f7c785db197b720771
piyushvers <piyushversatile96@gmail.com>
2022-01-21 00:23:09 -0600

    Configuration changes

--- src/main/java/com/usp/medicare/DoctorServiceApplication.java
+++ src/main/java/com/usp/medicare/DoctorServiceApplication.java
@@ -2,9 +2,10 @@ package com.usp.medicare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class DoctorServiceApplication {
 
 	public static void main(String[] args) {
